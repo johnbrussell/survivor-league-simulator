5f6000d30c67b83dc443774a43f85badd7fd9fdf
JBR <john.russell@tufts.edu>
2019-10-01 20:21:01 -0400

    Add dummy ScheduleGenerator

---
+++ survivor_league/schedule_generator.py
@@ -0,0 +1,10 @@
+NUM_CONFERENCES = 2
+NUM_DIVISIONS_PER_CONFERENCE = 4
+NUM_TEAMS_PER_DIVISION = 4
+
+
+class ScheduleGenerator:
+    def __init__(self):
+        self.num_conferences = NUM_CONFERENCES
+        self.num_divisions = NUM_DIVISIONS_PER_CONFERENCE
+        self.num_teams = NUM_TEAMS_PER_DIVISION
