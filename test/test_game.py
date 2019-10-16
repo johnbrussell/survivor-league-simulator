3bce30ccc49f6de88b1b71a5b18ece41c634d056
JBR <john.russell@tufts.edu>
2019-10-15 22:16:38 -0400

    team_1 -> home_team; team_2 -> away_team

--- test/test_game.py
+++ test/test_game.py
@@ -8,22 +8,22 @@ class TestGame(unittest.TestCase):
         team_1 = team.Team(name=1, conference=1, division=1)
         team_2 = team.Team(name=2, conference=1, division=1)
 
-        subject = game.Game(team_1=team_1, team_2=team_2)
+        subject = game.Game(home_team=team_1, away_team=team_2)
 
-        self.assertEqual((team_1.name, .5), subject.odds())
+        self.assertEqual(.5, subject.home_team_win_probability())
 
     def test_odds_favorable_for_favored_team(self):
         team_1 = team.Team(name=1, elo=1300, conference=1, division=1)
         team_2 = team.Team(name=2, elo=1200, conference=1, division=1)
 
-        subject = game.Game(team_1=team_1, team_2=team_2)
+        subject = game.Game(home_team=team_1, away_team=team_2)
 
-        self.assertGreater(subject.odds()[1], .5)
+        self.assertGreater(subject.home_team_win_probability(), .5)
 
     def test_odds_unfavorable_for_unfavored_team(self):
         team_1 = team.Team(name=1, elo=1300, conference=1, division=1)
         team_2 = team.Team(name=2, elo=1400, conference=1, division=1)
 
-        subject = game.Game(team_1=team_1, team_2=team_2)
+        subject = game.Game(home_team=team_1, away_team=team_2)
 
-        self.assertLess(subject.odds()[1], .5)
+        self.assertLess(subject.home_team_win_probability(), .5)
