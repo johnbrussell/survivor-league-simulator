21031e51283fba950abff7e0a80423fff415af25
JBR <john.russell@tufts.edu>
2019-10-15 22:16:48 -0400

    Generate schedule

--- survivor_league/schedule_generator.py
+++ survivor_league/schedule_generator.py
@@ -1,4 +1,6 @@
-from survivor_league import team
+import random
+
+from survivor_league import team, game
 
 
 NUM_CONFERENCES = 2
@@ -18,6 +20,7 @@ class ScheduleGenerator:
 
     def generate_schedule(self):
         teams = self._generate_teams()
+        return self._generate_schedule(teams)
 
     def _generate_teams(self):
         teams = list()
@@ -25,6 +28,33 @@ class ScheduleGenerator:
         for c in range(self.num_conferences):
             for d in range(self.num_divisions):
                 for t in range(self.num_teams):
-                    teams.append(team.Team(conference=c, division=d, name=t))
+                    teams.append(team.Team(conference=c, division=d, name=f"{c}-{d}-{t}"))
 
         return teams
+
+    def _generate_schedule(self, teams):
+        schedule = dict()
+
+        for week in range(self.num_weeks):
+            schedule[week] = self._generate_week(teams)
+
+        return schedule
+
+    def _generate_week(self, teams):
+        non_bye_teams = self._filter_bye_teams(teams)
+
+        home_teams = non_bye_teams[:int(len(non_bye_teams)/2)]
+        away_teams = non_bye_teams[int(len(non_bye_teams)/2):]
+        return [game.Game(home_team=h, away_team=a) for h, a in zip(home_teams, away_teams)]
+
+    def _filter_bye_teams(self, teams):
+        non_bye_teams = [t for t in teams if not self._determine_bye_week()]
+        if len(non_bye_teams) % 2 != 0:
+            random.shuffle(non_bye_teams)
+            non_bye_teams = non_bye_teams[1:]
+        return non_bye_teams
+
+    def _determine_bye_week(self):
+        chance_of_bye = float(self.num_byes) / self.num_weeks
+
+        return random.random() < chance_of_bye
--- test/test_schedule_generator.py
+++ test/test_schedule_generator.py
@@ -1,6 +1,8 @@
 import unittest
 
 from survivor_league import schedule_generator
+from survivor_league.schedule_generator import NUM_CONFERENCES, NUM_TEAMS_PER_DIVISION, NUM_DIVISIONS_PER_CONFERENCE, \
+    NUM_WEEKS, NUM_BYES
 
 
 class TestScheduleGenerator(unittest.TestCase):
@@ -9,8 +11,35 @@ class TestScheduleGenerator(unittest.TestCase):
 
         teams = subject._generate_teams()
 
-        expected_num_teams = schedule_generator.NUM_CONFERENCES * \
-                             schedule_generator.NUM_DIVISIONS_PER_CONFERENCE * \
-                             schedule_generator.NUM_TEAMS_PER_DIVISION
+        expected_num_teams = NUM_CONFERENCES * NUM_DIVISIONS_PER_CONFERENCE * NUM_TEAMS_PER_DIVISION
 
         self.assertEqual(expected_num_teams, len(teams))
+
+    def test__give_teams_byes_gives_the_right_number_of_teams_byes(self):
+        # This test is by design intentionally slightly flaky.  If you encounter an error with the assertions,
+        #  try running again.
+        subject = schedule_generator.ScheduleGenerator()
+
+        n_trials = 100
+        sum_num_non_bye_teams = 0
+        for i in range(n_trials):
+            non_bye = subject._filter_bye_teams(subject._generate_teams())
+            self.assertTrue(len(non_bye) % 2 == 0)
+            sum_num_non_bye_teams += len(non_bye)
+
+        num_non_bye_teams = float(sum_num_non_bye_teams) / n_trials
+
+        num_teams = NUM_CONFERENCES * NUM_DIVISIONS_PER_CONFERENCE * NUM_TEAMS_PER_DIVISION
+        expected_avg_num_non_byes = num_teams - float(NUM_BYES) / NUM_WEEKS * num_teams
+        allowable_num_bye_difference = 1
+        self.assertTrue(num_non_bye_teams > expected_avg_num_non_byes - allowable_num_bye_difference)
+        self.assertTrue(num_non_bye_teams < expected_avg_num_non_byes + allowable_num_bye_difference)
+
+    def test_generate_schedule_returns_correct_number_of_weeks_with_correct_number_of_games(self):
+        subject = schedule_generator.ScheduleGenerator()
+
+        result = subject.generate_schedule()
+
+        self.assertEqual(len(result), NUM_WEEKS)
+        for _week, games in result.items():
+            self.assertTrue(len(games) <= NUM_CONFERENCES * NUM_DIVISIONS_PER_CONFERENCE * NUM_TEAMS_PER_DIVISION / 2.0)
