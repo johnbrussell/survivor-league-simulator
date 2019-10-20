aa1e80a62e1c50a9058a9a29a8326e38431f037b
JBR <john.russell@tufts.edu>
2019-10-19 20:20:14 -0400

    Add Player.choose_team

--- survivor_league/player.py
+++ survivor_league/player.py
@@ -1,6 +1,13 @@
 class Player:
-    def __init__(self):
+    def __init__(self, strategy):
         self._teams_chosen = set()
+        self._strategy = strategy
+
+    def choose_team(self, games):
+        ranked_teams = self._strategy.rank_teams(games, self._teams_chosen)
+        if len(ranked_teams) == 0:
+            return None
+        return ranked_teams[0].name
 
     def have_chosen_team(self, team_name):
         return team_name in self._teams_chosen
--- test/test_player.py
+++ test/test_player.py
@@ -1,14 +1,26 @@
 import unittest
 
-from survivor_league import player
+from survivor_league import player, game, team
+from survivor_league.strategies import choose_randomly
 
 
 class TestPlayer(unittest.TestCase):
     def test_have_chosen_team(self):
-        subject = player.Player()
+        subject = player.Player(strategy=None)
 
         self.assertFalse(subject.have_chosen_team(team_name='Some random team name'))
 
         subject._teams_chosen.add('Chosen team name')
 
         self.assertTrue(subject.have_chosen_team(team_name='Chosen team name'))
+
+    def test_choose_team(self):
+        subject = player.Player(strategy=choose_randomly.ChooseRandomly())
+
+        teams = [team.Team(name=t, division=t, conference=t) for t in range(4)]
+        games = [game.Game(home_team=t1, away_team=t2) for t1, t2 in zip(teams[:int(len(teams)/2)],
+                                                                         teams[int(len(teams)/2):])]
+
+        choice = subject.choose_team(games)
+
+        self.assertTrue(len([t for t in teams if t.name == choice]) == 1)
