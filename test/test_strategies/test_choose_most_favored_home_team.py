5a21b5d0364d13656c3ab6909a414803704da7a4
JBR <john.russell@tufts.edu>
2019-11-29 12:49:22 -0500

    Fix bug in calculating how much each team is favored in choosing most favored home team

--- test/test_strategies/test_choose_most_favored_home_team.py
+++ test/test_strategies/test_choose_most_favored_home_team.py
@@ -8,18 +8,20 @@ class TestChooseMostFavoredHomeTeam(unittest.TestCase):
     def test_favor_most_favored_home_team(self):
         subject = choose_most_favored_home_team.ChooseMostFavoredHomeTeam()
 
-        team_1 = team.Team(name=1, conference=1, division=1, elo=1200)
+        team_1 = team.Team(name=1, conference=1, division=1, elo=1250)
         team_2 = team.Team(name=2, conference=2, division=2, elo=1300)
-        team_3 = team.Team(name=3, conference=3, division=3, elo=1100)
+        team_3 = team.Team(name=3, conference=3, division=3, elo=1600)
         team_4 = team.Team(name=4, conference=4, division=4, elo=1400)
-        teams = [team_1, team_2, team_3, team_4]
+        team_5 = team.Team(name=5, conference=5, division=5, elo=1500)
+        team_6 = team.Team(name=6, conference=6, division=6, elo=1100)
+        teams = [team_1, team_2, team_3, team_4, team_5, team_6]
         home_teams = teams[:int((len(teams)/2))]
         away_teams = teams[int(len(teams)/2):]
         games = [game.Game(home_team=h, away_team=a) for h, a in zip(home_teams, away_teams)]
 
         ranked_teams = subject.rank_teams(games, [])
 
-        self.assertListEqual(ranked_teams, [team_1, team_4, team_2, team_3])
+        self.assertListEqual(ranked_teams, [team_3, team_5, team_4, team_1, team_2, team_6])
 
     def test_ignore_ineligible_teams(self):
         subject = choose_most_favored_home_team.ChooseMostFavoredHomeTeam()
